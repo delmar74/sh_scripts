6884afaa7d3fcadd9056e5175c88b66a349ec700
Oleg <delmar74@rambler.ru>
2018-11-25 19:02:40 +0300

    add top

--- proc/ps.sh
+++ proc/ps.sh
@@ -1,4 +1,6 @@
+###################
 # get all process 
+###################
 ps aux 
 ps -ef
 ps -eF
@@ -15,6 +17,10 @@ ps -eF
 ## RSS - real memory size
 ## PSR - processor the command 
 
+
+#####################
+# Format columns for VIEW
+#####################
 # Get all procces with custom columns
 ps --format="uid uname %cpu %mem cmd"
 ps --format="uid uname %cpu cp cputime %mem uname thcount cmd" T 
@@ -23,6 +29,45 @@ ps --format="uid uname %cpu cp cputime %mem uname thcount cmd" T
 ## %cpu, %mem, arg, c (Processor utilization), cmd, comm (Command name only), cp (CPU Usage), cputime 
 ## egid (Effective group ID), egroup, etime (elapsed time), euid, euser, gid, group, pgid, pgrp (Process group),
 ## ppid, start, sz, thcount (Threads owned by the process), time, uid, uname
+
+##############################
+# Sort processes
+##############################
+ps -ef --sort user,pid
+
+## Available:
+## cmd (Executable name)
+## pcpu (CPU Utilization)
+## flags (Flags)
+## pgrp (Process group ID)
+## cutime (Cumulative user time)
+## cstime (Cumulative system time)
+## utime (User time)
+## pid (Process ID)
+## ppid (Parent process ID)
+## size (Size)
+## uid (User ID)
+## user (User Name)
+
+##############################
+# Search process by NAME/ID
+##############################
+# get several process
+ps -p <PID> <PID>
+
+# get PID by name (-f - full information)
+ps -C docker
+ps -f -C docker
+
+#######################
+# Other
+#######################
+
+# get current running processes
+ps r
+
+# get process for current TTY (terminal)
+ps T
 
 # get count of processes
 ps aux | wc -l
@@ -37,15 +82,3 @@ tty
 w
 who
 
-# get current running processes
-ps r
-
-# get process for current TTY (terminal)
-ps T
-
-# get several process
-ps -p <PID> <PID>
-
-# get PID by name (-f - full information)
-ps -C docker
-ps -f -C docker
